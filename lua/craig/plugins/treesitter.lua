50288929a39cdb6e40b4a287416ada54dfa0d9e8
Craig <58040967+Cra1g01@users.noreply.github.com>
2025-02-27 21:48:25 +0000

    feat: treesitter update

--- lua/craig/plugins/treesitter.lua
+++ lua/craig/plugins/treesitter.lua
@@ -10,6 +10,7 @@ return {
                     enable = true,
                     additional_vim_regex_highlighting = false,
                 },
+                indent = { enable = true },
                 ensure_installed = {
                     "c",
                     "go",
