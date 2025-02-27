70901604cf69a1af03529b75557288150f79be40
Craig <58040967+Cra1g01@users.noreply.github.com>
2025-02-27 21:48:36 +0000

    feat: telescope update

--- lua/craig/plugins/telescope.lua
+++ lua/craig/plugins/telescope.lua
@@ -1,13 +1,27 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-        vim.keymap.set("n", "<leader>ps", function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-    end,
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	config = function()
+		require("telescope").setup({
+			extentions = {
+				fzf = {},
+			},
+		})
+		require("telescope").load_extension("fzf")
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>pf", builtin.find_files)
+		vim.keymap.set("n", "<C-p>", builtin.git_files)
+		vim.keymap.set("n", "<leader>ps", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+		vim.keymap.set("n", "<leader>vh", builtin.help_tags)
+		vim.keymap.set("n", "<leader>en", function()
+			builtin.find_files({
+				cwd = vim.fn.stdpath("config"),
+			})
+		end)
+	end,
 }
