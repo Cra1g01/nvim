2ccce2abf0c4dc803a85a4d29e4de4ce1d6225a9
Craig <58040967+Cra1g01@users.noreply.github.com>
2026-04-04 12:10:00 +0100

    refactor: use spaces not tabs

--- lua/craig/plugins/telescope.lua
+++ lua/craig/plugins/telescope.lua
@@ -1,27 +1,29 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	config = function()
-		require("telescope").setup({
-			extentions = {
-				fzf = {},
-			},
-		})
-		require("telescope").load_extension("fzf")
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>pf", builtin.find_files)
-		vim.keymap.set("n", "<C-p>", builtin.git_files)
-		vim.keymap.set("n", "<leader>ps", function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
-		vim.keymap.set("n", "<leader>vh", builtin.help_tags)
-		vim.keymap.set("n", "<leader>en", function()
-			builtin.find_files({
-				cwd = vim.fn.stdpath("config"),
-			})
-		end)
-	end,
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  config = function()
+    require("telescope").setup({
+      extentions = {
+        fzf = {},
+      },
+    })
+    require("telescope").load_extension("fzf")
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>pf", builtin.find_files)
+    vim.keymap.set("n", "<C-p>", builtin.git_files)
+    vim.keymap.set("n", "<leader>pg", builtin.live_grep)
+    vim.keymap.set("n", "<leader>pt", builtin.treesitter)
+    vim.keymap.set("n", "<leader>ps", function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+    vim.keymap.set("n", "<leader>vh", builtin.help_tags)
+    vim.keymap.set("n", "<leader>en", function()
+      builtin.find_files({
+        cwd = vim.fn.stdpath("config"),
+      })
+    end)
+  end,
 }
