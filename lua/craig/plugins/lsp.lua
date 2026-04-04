7af270a0043bb49877d7c4cba2b9c091130ffd78
Craig <58040967+Cra1g01@users.noreply.github.com>
2026-04-04 12:16:03 +0100

    feat: update LSP and ft files

--- lua/craig/plugins/lsp.lua
+++ lua/craig/plugins/lsp.lua
@@ -5,10 +5,9 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "folke/neodev.nvim",
+      { "folke/lazydev.nvim", ft = "lua" },
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-      -- "WhoIsSethDanie/mason-tool-installer.nvim",
       "saghen/blink.cmp",
 
       { "j-hui/fidget.nvim", opts = {} },
@@ -18,20 +17,26 @@ return {
       "b0o/SchemaStore.nvim",
     },
     config = function()
-      require("neodev").setup()
-
-      -- local capabilities = nil
-      -- if pcall(require, "cmp_nvim_lsp") then
-      --   capabilities = require("cmp_nvim_lsp").default_capabilities()
-      -- end
-
       local capabilities = require("blink.cmp").get_lsp_capabilities()
-      local lspconfig = require "lspconfig"
 
       local servers = {
+        air = true,
+        r_language_server = true,
         bashls = true,
         gopls = true,
-        lua_ls = true,
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+              },
+            },
+          },
+        },
         rust_analyzer = {
           diagnostic = {
             refreshSupport = false,
@@ -42,7 +47,9 @@ return {
         cssls = true,
         terraformls = true,
 
-        pyright = true,
+        -- pyright = true,
+        ruff = true,
+        ty = true,
 
         ts_ls = true,
         tailwindcss = true,
@@ -66,8 +73,6 @@ return {
             },
           },
         },
-
-        r_language_server = true,
       }
 
       local servers_to_install = vim.tbl_filter(function(key)
@@ -83,12 +88,9 @@ return {
       local ensure_installed = {
         "stylua",
         "lua_ls",
-        "isort",
-        "black",
       }
 
       vim.list_extend(ensure_installed, servers_to_install)
-      -- require("mason-tool-installer").setup { ensure_installed = ensure_installed }
 
       for name, config in pairs(servers) do
         if config == true then
@@ -98,8 +100,9 @@ return {
           capabilities = capabilities,
         }, config)
 
-        lspconfig[name].setup(config)
+        vim.lsp.config(name, config)
       end
+      vim.lsp.enable(vim.tbl_keys(servers))
 
       local disable_semantic_tokens = {
         lua = true,
@@ -126,19 +129,15 @@ return {
           vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
           vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbold() end, opts)
           vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-          vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-          vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+          vim.keymap.set("n", "[d", vim.diagnostic.jump, opts)
+          vim.keymap.set("n", "]d", vim.diagnostic.jump, opts)
           vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 
           vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, opts)
           vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
 
           vim.keymap.set("n", "<space>f", function()
-            vim.notify "Formatting"
-            conform.format({
-              lsp_fallback = true,
-              quiet = true,
-            })
+            vim.lsp.buf.format()
           end, opts)
 
           local filetype = vim.bo[bufnr].filetype
@@ -148,19 +147,46 @@ return {
         end,
       })
 
+      vim.diagnostic.config({
+        virtual_text = true,
+        float = {
+          style = "minimal",
+          border = "rounded",
+        },
+      })
+      vim.keymap.set("n", "<leader>dl", function()
+        local config = vim.diagnostic.config() or {}
+        if config.virtual_text then
+          vim.diagnostic.config { virtual_text = false, virtual_lines = true }
+        else
+          vim.diagnostic.config { virtual_text = true, virtual_lines = false }
+        end
+      end)
+
       conform.setup {
+        lsp_format = "fallback",
+        quiet = false,
         formatters_by_ft = {
           lua = { "stylua" },
-          python = { "isort", "black" },
+          python = { "ruff_format", "ruff_organize_imports" },
           javascript = { "prettier" },
           typescript = { "prettier" },
           javascriptreact = { "prettier" },
           typescriptreact = { "prettier" },
+          r = { "air" },
+          svelte = { "prettier" },
+          sql = { "sqlfluff" },
+          rust = { "rustfmt" },
+          json = { "jq" },
+          yaml = { "prettier" },
         },
         formatters = {
-            black = {
-              prepend_args = { "--line-length", "79" },
-            },
+          lua = {
+            stylua = {
+              prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+            }
+          },
+
         },
       }
     end,
