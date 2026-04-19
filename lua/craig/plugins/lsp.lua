9403fb8af2b67e758eac59397fd2fd0c3a9395f8
Craig <58040967+Cra1g01@users.noreply.github.com>
2026-04-19 15:38:11 +0100

    fix: remove deprecated lsp jumps

--- lua/craig/plugins/lsp.lua
+++ lua/craig/plugins/lsp.lua
@@ -129,8 +129,6 @@ return {
           vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
           vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbold() end, opts)
           vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-          vim.keymap.set("n", "[d", vim.diagnostic.jump, opts)
-          vim.keymap.set("n", "]d", vim.diagnostic.jump, opts)
           vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 
           vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, opts)
