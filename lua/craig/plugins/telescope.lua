5713a8713287a7c737079585c4cf70009b008d33
Craig <58040967+Cra1g01@users.noreply.github.com>
2026-04-19 15:36:15 +0100

    fix: error in telescope

--- lua/craig/plugins/telescope.lua
+++ lua/craig/plugins/telescope.lua
@@ -6,11 +6,13 @@ return {
   },
   config = function()
     require("telescope").setup({
-      extentions = {
+      extensions = {
         fzf = {},
       },
     })
+
     require("telescope").load_extension("fzf")
+
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>pf", builtin.find_files)
     vim.keymap.set("n", "<C-p>", builtin.git_files)
