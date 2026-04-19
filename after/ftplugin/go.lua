a0092bea88a1e80571cae9bc1475ff68478108ed
Craig <58040967+Cra1g01@users.noreply.github.com>
2026-04-19 15:36:57 +0100

    fix: go format on save auto cmd and use conform

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,8 +1,16 @@
 vim.opt_local.expandtab = false
 
+local group = vim.api.nvim_create_augroup("GoFormatOnSave", { clear = false })
+vim.api.nvim_clear_autocmds({ group = group, buffer = 0 })
+
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.format()
+  group = group,
+  buffer = 0,
+  callback = function(args)
+    require("conform").format({
+      bufnr = args.buf,
+      lsp_format = "fallback",
+      timeout_ms = 500,
+    })
   end,
 })
