221cae6be70e7b9ec0f1b469beb8f8faa847f2d0
Craig <58040967+Cra1g01@users.noreply.github.com>
2026-05-16 23:23:24 +0100

    feat: improved trimends

--- after/plugin/trimends.lua
+++ after/plugin/trimends.lua
@@ -1,4 +1,11 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	command = [[%s/\s\+$//e]],
+  group = vim.api.nvim_create_augroup("TrimTrailingWhitespace", { clear = true }),
+  pattern = "*",
+  callback = function()
+    local view = vim.fn.winsaveview()
+
+    vim.cmd([[keepjumps keeppatterns %s/\s\+$//e]])
+
+    vim.fn.winrestview(view)
+  end,
 })
